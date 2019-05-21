dc2ed5b6be5c5af20fd9f70bfb7d10062dc9071b
Mota Wirkkala Jamille <s1900530@edu.bc.fi>
2019-05-21 18:30:03 +0300

    font size modified

--- echo.js/geolocation.js
+++ echo.js/geolocation.js
@@ -55,105 +55,105 @@ function initMap() {
                 lat: 60.170493,
                 lng: 24.938612
             },
-            content: '<h1> Fafas Sokos</h1><h3> Address: Postikatu, open 10-06</h3>'
+            content: '<h6> Fafas Sokos</h6><h6> Address: Postikatu, open 10-06</h6>'
         },
         {
             coords: {
                 lat: 60.167347,
                 lng: 24.941781
             },
-            content: '<h1>Hesburger</h1><h3> Adress: Mannerheimintie 8, open 9-24 </h3>'
+            content: '<h6>Hesburger</h6><h6> Adress: Mannerheimintie 8, open 9-24 </h6>'
         },
         {
             coords: {
                 lat: 60.166511,
                 lng: 24.932911
             },
-            content: '<h1>Eerikin Pippuri</h1><h3> Adress: Eerikinkatu 17, open 10-05 </h3>'
+            content: '<h6>Eerikin Pippuri</h6><h6> Adress: Eerikinkatu 17, open 10-05 </h6>'
         },
         {
             coords: {
                 lat: 60.169137,
                 lng: 24.932138
             },
-            content: '<h1>McDonalds</h1><h3> Address: Tennispalatsinaukio, open 0-24 </h3>'
+            content: '<h6>McDonalds</h6><h6> Address: Tennispalatsinaukio, open 0-24 </h6>'
         },
         {
             coords: {
                 lat: 60.165436,
                 lng: 24.948339
             },
-            content: '<h1>Hesburger</h1><h3> Address: Kasarminkatu 19, open 11:30-05 </h3>'
+            content: '<h6>Hesburger</h6><h6> Address: Kasarminkatu 19, open 11:30-05 </h6>'
         },
         {
             coords: {
                 lat: 60.190044,
                 lng: 24.917421
             },
-            content: '<h1>McDonalds</h1><h3> Address: Mannerheimintie 105, open 0-24</h3>'
+            content: '<h6>McDonalds</h6><h6> Address: Mannerheimintie 105, open 0-24</h6>'
         },
         {
             coords: {
                 lat: 60.305529,
                 lng: 24.963993
             },
-            content: '<h1>Alepa 24h Airport </h1><h3> Address: Airport Terminal 2, open 0-24</h3>'
+            content: '<h6>Alepa 24h Airport </h6><h6> Address: Airport Terminal 2, open 0-24</h6>'
         },
         {
             coords: {
                 lat: 60.168314,
                 lng: 24.935178
             },
-            content: '<h1>Sky Express </h1><h3> Address: Annankatu 31-33, open 10-05</h3>'
+            content: '<h6>Sky Express </h6><h6> Address: Annankatu 31-33, open 10-05</h6>'
         },
         {
             coords: {
                 lat: 60.194979,
                 lng: 25.029869
             },
-            content: '<h1>Alepa 24 h </h1><h3> Address: Herttoniemi metro station, open 0-24 </h3>'
+            content: '<h6>Alepa 24 h </h6><h6> Address: Herttoniemi metro station, open 0-24 </h6>'
         },
         {
             coords: {
                 lat: 60.170024,
                 lng: 24.93774
             },
-            content: '<h1> Grill Kiosk </h1><h3> Address: Mannerheimintie 22, open 22-05</h3>'
+            content: '<h6> Grill Kiosk </h6><h6> Address: Mannerheimintie 22, open 22-05</h6>'
         },
         {
             coords: {
                 lat: 60.158334,
                 lng: 24.875831
             },
-            content: '<h1> Kaizun Bistro</h1><h3>Address: Gyldenintie 2, open 21-04</h3>'
+            content: '<h6> Kaizun Bistro</h6><h3>Address: Gyldenintie 2, open 21-04</h6>'
         },
         {
             coords: {
                 lat: 60.170812,
                 lng: 24.940265
             },
-            content: '<h1> Black Grill & Cafe </h1><h3> Address: Railway station, open 10-24 </h3>'
+            content: '<h6> Black Grill & Cafe </h6><h6> Address: Railway station, open 10-24 </h6>'
         },
         {
             coords: {
                 lat: 60.170382,
                 lng: 24.921072
             },
-            content: '<h1> ABC Kylmäasema </h1><h3> Address: Mechelininkatu 5, open 0-24 </h3>'
+            content: '<h6> ABC Kylmäasema </h6><h6> Address: Mechelininkatu 5, open 0-24 </h6>'
         },
         {
             coords: {
                 lat: 60.180336,
                 lng: 24.9489
             },
-            content: '<h1> S-market </h1><h3> Address: Siltasaarenkatu 18-20, open 0-24 </h3>'
+            content: '<h6> S-market </h6><h6> Address: Siltasaarenkatu 18-20, open 0-24 </h6>'
         },
         {
             coords: {
                 lat: 60.170515,
                 lng: 24.938274
             },
-            content: '<h1> S-market </h1><h3> Address: Mannerheimintie 9, open: 0-24 </h3>'
+            content: '<h6> S-market </h6><h6> Address: Mannerheimintie 9, open: 0-24 </h6>'
         },
     ];
 
